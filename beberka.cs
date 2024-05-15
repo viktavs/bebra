e6dcbfd581c1fe275bb48fb41f1ec22d059b39dd
viktavs <79546757+viktavs@users.noreply.github.com>
2024-05-15 14:43:51 -0500

    Update beberka.cs

--- beberka.cs
+++ beberka.cs
@@ -1,1 +1,10 @@
-Console.WriteLine("meow");
+// 1000 random ints with an average
+
+Random myRan = new Random();
+int[] bebra = new bebra{1000};
+
+for (int i = 0; i < bebra.Length; i++) {
+  bebra[i] = myRan.Next(1, 100);
+}
+
+Console.Writeline(bebra.Average());
